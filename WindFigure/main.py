708ebf8f7d45b0c51a7ac8b80685b54e2e0b40d3
Wangkaiwei233 <w1141125307@hotmail.com>
2025-02-24 12:22:55 +1100

    Clean up requirement documentation and remove unused files

--- WindFigure/main.py
+++
@@ -1,8 +0,0 @@
-print("Hello, World!")
-import xarray as xr
-import numpy as np
-import datetime as dt
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-import cartopy.mpl.ticker as cticker
-import matplotlib.pyplot as plt
